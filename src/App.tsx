3c9e3829c50037c2389720967ad5bd1005e219cc
Bongane Lebopo <blebopo@dvtsoftware.com>
2026-03-03 14:03:39 +0200

    Scaffolding: Basic navigation using react router initializes.

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
 import { useState } from 'react'
 import './App.css'
+import { BrowserRouter, Route, Routes } from 'react-router'
+import PageNotFoundPage from './pages/PageNotFound'
+import HomePage from './pages/Home'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+    <BrowserRouter>
+      <Routes >
+        <Route path="/" element={<HomePage/>}/>
+        <Route path="*" element={<PageNotFoundPage/>}/>
+      </Routes>
+    </BrowserRouter>
       
     </>
   )
