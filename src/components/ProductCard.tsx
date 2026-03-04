12805687a00988e876dce737962b8fd2baf15683
Bongane Lebopo <blebopo@dvtsoftware.com>
2026-03-04 19:56:59 +0200

    Semantics: Divided each product property with an appropriate div.

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -9,35 +9,32 @@ export default function ProductCardComponent() {
   return (
     <>
       <article className="product-card">
-          <img
-            src={product.image}
-            alt="Product image"
-          />
+        <img src={product.image} alt={product.title} />
 
-          <div className="product-price">
-            <p>R {product.price}</p>
-          </div>
+        <div className="product-price">
+          <p>R {product.price}</p>
+        </div>
 
-          <div className="product-title">
-            <p>{product.title}</p>
-          </div>
+        <div className="product-title">
+          <p>{product.title}</p>
+        </div>
 
-          <div className="product-description">
-            <p>{product.description}</p>
-          </div>
-          
-          <div className="product-category">
-            <p>{product.category}</p>
-          </div>
+        <div className="product-description">
+          <p>{product.description}</p>
+        </div>
 
-          <div className="product-rating">
-            <p>{product.rating.rate}</p>
-          </div>
+        <div className="product-category">
+          <p>{product.category}</p>
+        </div>
 
-          <div className="product-rating-count">
-            <p>{product.rating.count}</p>
-          </div>
-          
+        <div className="product-rating">
+          <p>{product.rating.rate}</p>
+        </div>
+
+        <div className="product-rating-count">
+          <p>{product.rating.count}</p>
+        </div>
+
         <button
           className="add-to-cart-button"
           onClick={() => handleOnClick(product)}
