3502550b13a7cf9aeaaf7c9f11ea38bc00e67b5e
Bongane Lebopo <blebopo@dvtsoftware.com>
2026-03-04 23:48:02 +0200

    Style: Cart items properly spaced out

--- src/components/CartSummary.tsx
+++ src/components/CartSummary.tsx
@@ -1,3 +1,5 @@
+import "../styles/CartSummary.css";
+
 export default function CartSummaryComponent() {
   return (
     <aside className="cart-summary">
@@ -20,7 +22,23 @@ export default function CartSummaryComponent() {
               Remove
             </button>
           </div>
-          <span>R item total</span>
+          <span className="cart-totals">R item total</span>
+        </li>
+        <li key={1} className="cart-item">
+          <span>item title</span>
+          <div className="cart-summary-controls">
+            <button onClick={() => {}} className="decrease-item-button">
+              -
+            </button>
+            <span>item quantity</span>
+            <button onClick={() => {}} className="add-item-button">
+              +
+            </button>
+            <button onClick={() => {}} className="remove-item-button">
+              Remove
+            </button>
+          </div>
+          <span className="cart-totals">R item total</span>
         </li>
       </ul>
     </aside>
